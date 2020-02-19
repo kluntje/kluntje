71577c2eadc09a6c8e7cc8591c0c1ca238d12e99
Frederik Riewerts <friewerts@pro-vision.de>
2020-02-19 15:36:34 +0100

    PVFE-18 | added NodeList to parameter types in addClass and removeClass

--- packages/js-utils/src/dom-helpers/lib/removeClass.ts
+++ packages/js-utils/src/dom-helpers/lib/removeClass.ts
@@ -4,13 +4,17 @@
  * @param {Element|Iterable<Element>} elements
  * @param {...string} classNames
  */
-export const removeClass = (elements: Element | Iterable<Element>, ...classNames: string[]): void => {
+export const removeClass = (elements: Element | Iterable<Element> | NodeListOf<Element>, ...classNames: string[]): void => {
   if (elements === undefined || classNames.length === 0) {
     return;
   }
 
   if (elements instanceof Element) {
     elements = [elements];
+  }
+
+  if (elements instanceof NodeList) {
+    elements = Array.from(elements);
   }
 
   for (const element of elements) {
