ed64bdfd6952ee2c431383799da139d727ac24bb
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-02-29 15:33:08 +0100

    docs(js-utils): updated js-docs of date-helpers
    updated js-docs and added examples to all date-helpers
    
    re #5

--- packages/js-utils/src/date-helpers/lib/addLeadingZero.ts
+++ packages/js-utils/src/date-helpers/lib/addLeadingZero.ts
@@ -2,6 +2,9 @@
  * Optionally Adds leading Zero to Numbers < 10
  * @param {number} inNumber
  * @returns {string}
+ * @example
+ * const today = new Date();
+ * const formattedDateSting = `${addLeadingZero(today.getDate())}.${addLeadingZero(today.getMonth() + 1)}.${today.getFullYear()}`;
  */
 export const addLeadingZero = (inNumber: number): string => {
   return `${inNumber < 10 ? "0" : ""}${inNumber.toString()}`;
