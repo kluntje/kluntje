159c96d7cfaa311d94f10209e1a8ad7f018ddb00
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-02-29 14:52:00 +0100

    docs(js-utils): updated jsdocs and added examples to array-helpers
    added correct typings to jsdocs and  examples to all array-helpers
    
    re #5

--- packages/js-utils/src/array-helpers/lib/mergeArraysBy.ts
+++ packages/js-utils/src/array-helpers/lib/mergeArraysBy.ts
@@ -1,9 +1,59 @@
 /**
  * merge two given arrays by the given checker function
- * @param {Array<any>} array1
- * @param {Array<any>} array2
+ * @param {Array<T>} array1
+ * @param {Array<T>} array2
  * @param {Function} checker - if this function returns true for a specific element combination the elements are getting merged
- * @returns {Array<any>}
+ * @returns {Array<T>}
+ * @example
+ * const defaultUsers = [
+ *   {
+ *     name: "Admin",
+ *     mail: "admin@company.com"
+ *   },
+ *   {
+ *     name: "CI",
+ *     mail: "ci@company.com"
+ *   }
+ * ];
+ * 
+ * const projectUsers = [
+ *   {
+ *     name: "Admin",
+ *     mail: "admin@company.com"
+ *   },
+ *   {
+ *     name: "User1",
+ *     mail: "user-one@company.com"
+ *   },
+ *   {
+ *     name: "User2",
+ *     mail: "user-two@company.com"
+ *   }
+ * ];
+ * 
+ * const userList = mergeArraysBy(defaultUsers, projectUsers, (defaultUser, array) => {
+ *   return !array.some((projectUser) => projectUser.mail === defaultUser.mail)
+ * })
+ * 
+ * // userList
+ * // [
+ * //   {
+ * //     "name": "CI",
+ * //     "mail": "ci@company.com"
+ * //   },
+ * //   {
+ * //     "name": "Admin",
+ * //     "mail": "admin@company.com"
+ * //   },
+ * //   {
+ * //     "name": "User1",
+ * //     "mail": "user-one@company.com"
+ * //   },
+ * //   {
+ * //     "name": "User2",
+ * //     "mail": "user-two@company.com"
+ * //   }
+ * // ] 
  */
 export const mergeArraysBy = <T>(
   array1: Array<T>,
