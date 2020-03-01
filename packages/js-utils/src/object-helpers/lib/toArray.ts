a6ba578bf280d995bbf1811e64fb419c54d19c83
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-03-01 15:15:18 +0100

    docs(js-utils): updated jsdocs of all object-helpers
    updated jsdocs of all object-helpers and added examples
    
    re #5

--- packages/js-utils/src/object-helpers/lib/toArray.ts
+++ packages/js-utils/src/object-helpers/lib/toArray.ts
@@ -1,7 +1,10 @@
 /**
  * returns the argument wrapped in an array if it isn't array itself
- * @param {*} arg
- * @returns {Array}
+ * @param {T | Array<T>} arg
+ * @returns {Array<T>}
+ * @example
+ * const apple = "Apple";
+ * const fruits = toArray(apple); // ["Apple"] 
  */
 export const toArray = <T>(arg: T | Array<T>): Array<T> => {
   return Array.isArray(arg) ? arg : [arg];
--- packages/js-utils/src/object-helpers/lib/toString.ts
+++ packages/js-utils/src/object-helpers/lib/toString.ts
@@ -2,6 +2,8 @@
  * returns stringified value for the given argument
  * @param {*} arg
  * @returns {string}
+ * @example
+ * const submitData = toString(formData);
  */
 export const toString = (arg: any): string => {
   if (typeof arg === 'object' && arg !== null) {
