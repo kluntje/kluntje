18c36aaf698022c030c85cc0825345075de6c376
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-03-01 17:47:08 +0100

    docs(js-utils): updated jsdocs of string-helpers
    updated jsdocs of string-helpers and added examples
    
    re #5

--- packages/js-utils/src/string-helpers/lib/getWordCount.ts
+++ packages/js-utils/src/string-helpers/lib/getWordCount.ts
@@ -1,8 +1,10 @@
-
 /**
  * returns number of words in a given text
  * @param {string} text
  * @returns {number}
+ * @example
+ * const article = find(document, 'aricle');
+ * const articleWords = getWordCount(article.innerText);
  */
 export const getWordCount = (text: string): number => {
   return text.split(' ').length;
