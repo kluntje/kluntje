ccd7822ffc11a0778f5a710002c5e5356c829dd6
mbehzad <m_online_net@yahoo.com>
2020-03-04 16:27:19 +0100

    style: remove tests skips

--- packages/js-utils/src/string-helpers/tests/toCamelCase.test.ts
+++ packages/js-utils/src/string-helpers/tests/toCamelCase.test.ts
@@ -1,5 +1,5 @@
 import { toCamelCase } from '../lib/toCamelCase';
 
-test.only('(toCamelCase) should convert text to camelCase', () => {
+test('(toCamelCase) should convert text to camelCase', () => {
   expect(toCamelCase('http2 server-push')).toBe('http2ServerPush');
 });
