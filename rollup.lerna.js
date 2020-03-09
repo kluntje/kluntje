e3a3de4055b5337840a39b40911bec3e71e428f8
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-03-09 11:51:32 +0100

    build(js-utils): minify js-utils lib
    minify js-utils to have minimal footprint
    
    fix #32

--- rollup.lerna.js
+++ rollup.lerna.js
@@ -1,22 +1,22 @@
-import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 export const generateRollupConfig = (pkg, inputPath) => ({
   input: inputPath, // our source file
   output: [
     {
       file: pkg.main,
-      format: "cjs"
+      format: 'cjs',
     },
     {
       file: pkg.module,
-      format: "es" // the preferred format
-    }
+      format: 'es', // the preferred format
+    },
   ],
   external: [...Object.keys(pkg.dependencies || {})],
   plugins: [
     typescript({
-      typescript: require("typescript")
+      typescript: require('typescript'),
     }),
-    terser() // minifies generated bundles
-  ]
+    terser(), // minifies generated bundles
+  ],
 });
