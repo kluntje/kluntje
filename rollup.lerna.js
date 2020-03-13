3a843be767ce589fec8446c21ae0a1797c47d88e
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-03-13 12:52:25 +0100

    build(root): added babel transpiling to rollup build tasks
    added babel transpiling to rollup build tasks, to provide transpiled versions of the packages
    
    re #36

--- rollup.lerna.js
+++ rollup.lerna.js
@@ -1,4 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import babel from 'rollup-plugin-babel';
+import path from 'path';
 import { terser } from 'rollup-plugin-terser';
 export const generateRollupConfig = (pkg, inputPath) => ({
   input: inputPath, // our source file
@@ -16,6 +19,48 @@ export const generateRollupConfig = (pkg, inputPath) => ({
   plugins: [
     typescript({
       typescript: require('typescript'),
+    }),
+    babel({
+      extensions: [...DEFAULT_EXTENSIONS, '.ts'],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: [
+              'last 2 firefox versions',
+              'last 2 chrome versions',
+              'last 2 and_chr versions',
+              'last 2 safari versions',
+              'last 2 ios_saf versions',
+              'last 2 edge versions',
+            ],
+          },
+        ],
+        '@babel/preset-typescript',
+      ],
+      plugins: [
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true,
+          },
+        ],
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            corejs: false,
+            regenerator: true,
+            useESModules: true,
+            helpers: false,
+            absoluteRuntime: path.dirname(require.resolve('@babel/runtime/package.json')),
+          },
+        ],
+        '@babel/plugin-syntax-dynamic-import',
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        ['@babel/plugin-transform-classes', { loose: true }],
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-optional-chaining',
+      ],
     }),
     terser(), // minifies generated bundles
   ],
