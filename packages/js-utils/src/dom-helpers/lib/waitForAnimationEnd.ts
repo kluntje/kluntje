cd8e4a980c67e4eae90b11d6485d25b147ac5c20
mbehzad <mbehzad@pro-vision.de>
2020-11-14 13:30:12 +0100

    docs(js-utils): add example to jsdoc
    ignore animation/tranistion from child elements

--- packages/js-utils/src/dom-helpers/lib/waitForAnimationEnd.ts
+++ packages/js-utils/src/dom-helpers/lib/waitForAnimationEnd.ts
@@ -1,6 +1,17 @@
 /**
  * returns a promise which resolves after the animationend event
  *
+ * @example
+ * ```
+ *   el.classList.add("hide");
+ *   await waitForAnimationEnd(el, "fade-out");
+ *   el.parentElement.removeChild(el);
+ *   // css:
+ *   // .hide {
+ *   //   animation: fade-out 0.5s forwards;
+ *   // }
+ * ```
+ *
  * @param {HTMLElement|SVGElement} el - DOM Element which has the css animation
  * @param {string} [animationName] - keyframes' name. e.g. "slideOut"
  * @returns {Promise}
@@ -8,6 +19,8 @@
 export function waitForAnimationEnd(el: HTMLElement, animationName?: string): Promise<AnimationEvent> {
   return new Promise((resolve, _reject) => {
     el.addEventListener<'animationend'>('animationend', function callBack(e: AnimationEvent) {
+      // ignore child animations
+      if (e.target !== el) return;
       // if animationName is given then ignore animation for other keyframes
       if (animationName && e.animationName !== animationName) {
         return;
--- packages/js-utils/src/dom-helpers/lib/waitForTransitionEnd.ts
+++ packages/js-utils/src/dom-helpers/lib/waitForTransitionEnd.ts
@@ -1,6 +1,15 @@
 /**
  * returns a promise which resolves after the `transitionend` event
  *
+ * @example
+ * ```
+ *   menu.classList.add("open");
+ *   await waitForTransitionEnd(menu, "transform");
+ *   input.classList.add("visible");
+ *   await waitForTransitionEnd(input, "opacity");
+ *   input.focus();
+ * ```
+ *
  * @param {HTMLElement|SVGElement} el - DOM Element which has the css transition
  * @param {string} [propertyName] - transition's propertyName. e.g. "width"
  * @returns {Promise}
@@ -9,6 +18,8 @@
 export function waitForTransitionEnd(el: HTMLElement, propertyName?: string): Promise<TransitionEvent> {
   return new Promise((resolve, _reject) => {
     el.addEventListener<'transitionend'>('transitionend', function callBack(e: TransitionEvent) {
+      // ignore child transitions
+      if (e.target !== el) return;
       // if propertyName is given then ignore trasnition on other properties
       if (propertyName && e.propertyName !== propertyName) {
         return;
