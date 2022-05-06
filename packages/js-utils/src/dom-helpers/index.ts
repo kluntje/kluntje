628403dbf8288d4563fd938a5dcc84f81de28783
Frederik Riewerts <frederik.riewerts@gmail.com>
2022-05-06 15:14:07 +0200

    Revert "feat(js-utils): added waitForInitialization helper"
    This reverts commit ce4a694fc7f66626cabc81e5ec3de856c8997c0c.

--- packages/js-utils/src/dom-helpers/index.ts
+++ packages/js-utils/src/dom-helpers/index.ts
@@ -18,6 +18,5 @@ export { waitFor } from './lib/waitFor';
 export { Context } from './lib/Context';
 export { waitForEvent } from './lib/waitForEvent';
 export { waitForAnimationEnd } from './lib/waitForAnimationEnd';
-export { waitForInitialization } from './lib/waitForInitialization';
 export { waitForTransitionEnd } from './lib/waitForTransitionEnd';
 export { getCurrentMQ, MQDefinition } from './lib/getCurrentMQ';
