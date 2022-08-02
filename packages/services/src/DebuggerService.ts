638f9b44927b255d0b6cd4992d63228a92a9f8ad
mbehzad <mehran.behzad@diva-e.com>
2022-08-02 11:42:28 +0200

    refactor(services): provide better trace output, provide support for all console methods

--- packages/services/src/DebuggerService.ts
+++ packages/services/src/DebuggerService.ts
@@ -1,24 +1,15 @@
 import URLSearchParamsService from './URLSearchParamsService';
 
-class DebuggerService {
-  get debugModeActive() {
-    return URLSearchParamsService.get('js-debug') !== null;
-  }
-
-  log(message?: any, ...args: any[]) {
-    if (this.debugModeActive === false) return;
-    console.log(message, ...args);
-  }
-
-  warn(message?: any, ...args: any[]) {
-    if (this.debugModeActive === false) return;
-    console.warn(message, ...args);
-  }
-
-  error(message?: any, ...args: any[]) {
-    if (this.debugModeActive === false) return;
-    console.error(message, ...args);
-  }
+function debugModeActive() {
+  return URLSearchParamsService.get('js-debug') !== null;
 }
 
-export default new DebuggerService();
+const handler = {
+  get(target: Console, prop: keyof Console) {
+    if (debugModeActive()) return target[prop];
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    else return () => {};
+  },
+};
+
+export default new Proxy(console, handler);
